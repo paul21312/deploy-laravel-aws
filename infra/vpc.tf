0c736e81e271c9f1561a6546fe0c55eab95bfc07
root <root@DESKTOP-H3MUMCV.localdomain>
2025-09-18 00:14:04 +0800

    fix format

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -12,15 +12,16 @@ resource "aws_vpc" "this" {
   }
 }
 
+# use first 2 AZ names as strings
 locals {
-  azs = [0,1]
+  azs = slice(data.aws_availability_zones.azs.names, 0, 2)
 }
 
 resource "aws_subnet" "public" {
   for_each                = toset(local.azs)
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 8, each.key)
-  availability_zone       = data.aws_availability_zones.azs.names[each.key]
+  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 8, index(local.azs, each.key))
+  availability_zone       = each.key
   map_public_ip_on_launch = true
 
   tags = {
@@ -36,13 +37,13 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this.id
 
   route {
-    cidr_block = "0.0.0.0/0" 
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  for_each      = aws_subnet.public
-  subnet_id     = each.value.id
+  for_each       = aws_subnet.public
+  subnet_id      = each.value.id
   route_table_id = aws_route_table.public.id
 }
